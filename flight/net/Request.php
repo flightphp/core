37c50c5eeeef4cb3ef981954a1a61f291c447d33
Mike Cao <mike@mikecao.com>
2012-12-06 09:32:07 -0800

    Added method to get real IP address

--- flight/net/Request.php
+++ flight/net/Request.php
@@ -46,7 +46,7 @@ class Request {
                 'base' => str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME'])),
                 'method' => $_SERVER['REQUEST_METHOD'],
                 'referrer' => isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '',
-                'ip' => $_SERVER['REMOTE_ADDR'],
+                'ip' => $this->getIpAddress(),
                 'ajax' => isset($_SERVER['HTTP_X_REQUESTED_WITH']) ? ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') : false,
                 'scheme' => $_SERVER['SERVER_PROTOCOL'],
                 'user_agent' => $_SERVER['HTTP_USER_AGENT'],
@@ -102,6 +102,33 @@ class Request {
         }
 
         return $params;
+    }
+
+    /**
+     * Gets the real remote IP address.
+     *
+     * @return string IP address
+     */
+    private function getIpAddress() {
+        static $vars = array(
+            'HTTP_CLIENT_IP',
+            'HTTP_X_FORWARDED_FOR',
+            'HTTP_X_FORWARDED',
+            'HTTP_X_CLUSTER_CLIENT_IP',
+            'HTTP_FORWARDED_FOR',
+            'HTTP_FORWARDED',
+            'REMOTE_ADDR'
+        );
+
+        foreach ($vars as $key) {
+            if (array_key_exists($key, $_SERVER) === true) {
+                foreach (explode(',', $_SERVER[$key]) as $ip) {
+                    if (filter_var($ip, FILTER_VALIDATE_IP) !== false) {
+                        return $ip;
+                    }
+                }
+            }
+        }
     }
 }
 ?>
