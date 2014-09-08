e6ed90e3fb62f8c1851b237a4379a80f85cd11d3
Lars-Jørgen Kristiansen <larsjk.84@gmail.com>
2014-09-08 15:52:19 +0200

    Added more HTTP status codes

--- flight/net/Response.php
+++ flight/net/Response.php
@@ -33,6 +33,10 @@ class Response {
      * @var array HTTP status codes
      */
     public static $codes = array(
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+
         200 => 'OK',
         201 => 'Created',
         202 => 'Accepted',
@@ -40,6 +44,7 @@ class Response {
         204 => 'No Content',
         205 => 'Reset Content',
         206 => 'Partial Content',
+        207 => 'Multi-Status',
 
         300 => 'Multiple Choices',
         301 => 'Moved Permanently',
@@ -47,10 +52,12 @@ class Response {
         303 => 'See Other',
         304 => 'Not Modified',
         305 => 'Use Proxy',
+        306 => 'Switch Proxy',
         307 => 'Temporary Redirect',
 
         400 => 'Bad Request',
         401 => 'Unauthorized',
+        402 => 'Payment Required',
         403 => 'Forbidden',
         404 => 'Not Found',
         405 => 'Method Not Allowed',
@@ -66,14 +73,25 @@ class Response {
         415 => 'Unsupported Media Type',
         416 => 'Requested Range Not Satisfiable',
         417 => 'Expectation Failed',
-        418 => 'Permanent Redirect',
+        418 => 'I\'m a teapot',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        425 => 'Unordered Collection',
+        426 => 'Upgrade Required',
+        449 => 'Retry With',
+        450 => 'Blocked by Windows Parental Controls',
 
         500 => 'Internal Server Error',
         501 => 'Not Implemented',
         502 => 'Bad Gateway',
         503 => 'Service Unavailable',
         504 => 'Gateway Timeout',
-        505 => 'HTTP Version Not Supported'
+        505 => 'HTTP Version Not Supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        509 => 'Bandwidth Limit Exceeded',
+        510 => 'Not Extended'
     );
 
     /**
