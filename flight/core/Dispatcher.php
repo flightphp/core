f2eb4575648342ffbdf7c1d90d9874011845dc8c
J. Lueken <codezen@posteo.us>
2021-12-22 09:57:22 -0500

    Fix Dispatcher::invokeMethod to allow for named params when calling Flight::<staticMethod>()

--- flight/core/Dispatcher.php
+++ flight/core/Dispatcher.php
@@ -209,38 +209,7 @@ class Dispatcher
      */
     public static function invokeMethod($func, array &$params = [])
     {
-        [$class, $method] = $func;
-
-        $instance = \is_object($class);
-
-        switch (\count($params)) {
-            case 0:
-                return ($instance) ?
-                    $class->$method() :
-                    $class::$method();
-            case 1:
-                return ($instance) ?
-                    $class->$method($params[0]) :
-                    $class::$method($params[0]);
-            case 2:
-                return ($instance) ?
-                    $class->$method($params[0], $params[1]) :
-                    $class::$method($params[0], $params[1]);
-            case 3:
-                return ($instance) ?
-                    $class->$method($params[0], $params[1], $params[2]) :
-                    $class::$method($params[0], $params[1], $params[2]);
-            case 4:
-                return ($instance) ?
-                    $class->$method($params[0], $params[1], $params[2], $params[3]) :
-                    $class::$method($params[0], $params[1], $params[2], $params[3]);
-            case 5:
-                return ($instance) ?
-                    $class->$method($params[0], $params[1], $params[2], $params[3], $params[4]) :
-                    $class::$method($params[0], $params[1], $params[2], $params[3], $params[4]);
-            default:
-                return \call_user_func_array($func, $params);
-        }
+        return \call_user_func_array($func, $params);
     }
 
     /**
