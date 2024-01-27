f8a3b841cf531621b8cdb812db71de1139b577a7
fadrian06 <franyeradriansanchez@gmail.com>
2024-01-26 20:25:27 -0400

    Flight and Engine docblocks refactored

--- flight/autoload.php
+++ flight/autoload.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * Flight: An extensible micro-framework.
- *
- * @copyright   Copyright (c) 2013, Mike Cao <mike@mikecao.com>
- * @license     MIT, http://flightphp.com/license
- */
-
 declare(strict_types=1);
 
 use flight\core\Loader;
