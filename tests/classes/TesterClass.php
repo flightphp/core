701e0e73cef8622074214883febeb95e052af1a4
fadrian06 <franyeradriansanchez@gmail.com>
2026-03-26 11:59:42 -0400

    fix TypeError

--- tests/classes/TesterClass.php
+++ tests/classes/TesterClass.php
@@ -6,20 +6,20 @@ namespace tests\classes;
 
 final class TesterClass
 {
-    public string $param1;
-    public string $param2;
-    public string $param3;
-    public string $param4;
-    public string $param5;
-    public string $param6;
+    public ?string $param1;
+    public ?string $param2;
+    public ?string $param3;
+    public ?string $param4;
+    public ?string $param5;
+    public ?string $param6;
 
     public function __construct(
-        string $param1,
-        string $param2,
-        string $param3,
-        string $param4,
-        string $param5,
-        string $param6
+        ?string $param1,
+        ?string $param2,
+        ?string $param3,
+        ?string $param4,
+        ?string $param5,
+        ?string $param6
     ) {
         $this->param1 = $param1;
         $this->param2 = $param2;
